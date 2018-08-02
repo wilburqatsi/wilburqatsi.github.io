730de83b75e7d1ead4ffb9cd6915701ef01e4054
Will <wilburqatsi@gmail.com>
2018-08-02 18:10:40 -0500

    fixed arrow spacing in mobile devices

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,6 @@
 // Smoothly scroll to projects section upon clicking on arrow
 
-$("#arrow").click(function(){
+$(".arrow").click(function(){
 	$("html, body").animate({
 		scrollTop: $("#projects").offset().top
 	}, 1000);
